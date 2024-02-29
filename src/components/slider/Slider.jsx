7db2bf1f4bb8184563d483fea6a1ff701f86c844
aishayousef123 <you@example.com>
2024-03-01 01:23:56 +0200

    Any message - what you have done since your last commit

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import featured from '../../assets/images/featured.webp'
 import featured2 from '../../assets/images/featured2.webp'
 import featured3 from '../../assets/images/featured3.webp'
@@ -8,13 +12,19 @@ import featured3 from '../../assets/images/featured3.webp'
 function Slider() {
   return (
     <Swiper
-    spaceBetween={50}
-    slidesPerView={1}
-     >
-    <SwiperSlide></SwiperSlide>
-    <SwiperSlide> <img src={featured} alt='' className='featured' style={{width:"50%"}}/></SwiperSlide>
-    <SwiperSlide><img src={featured2} alt='' className='featured2' style={{width:"50%"}}/></SwiperSlide>
-    <SwiperSlide><img src={featured3} alt='' className='featured3' style={{width:"50%"}}/></SwiperSlide>
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+       <SwiperSlide> <img src={featured} alt='' className='featured' style={{width:"100%"}}/></SwiperSlide>
+    <SwiperSlide><img src={featured2} alt='' className='featured2' style={{width:"100%"}}/></SwiperSlide>
+    <SwiperSlide><img src={featured3} alt='' className='featured3' style={{width:"100%"}}/></SwiperSlide>
   </Swiper>
   )
 }
