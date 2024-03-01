b3f302d9ef20328d589e3cfee24b072a9c3eeb87
aishayousef123 <you@example.com>
2024-03-01 02:12:33 +0200

    Any message - what you have done since your last commit

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+//import { EffectFade } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
@@ -14,7 +16,8 @@ function Slider() {
     <Swiper
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
+           spaceBetween={50}
+           //effect="fade"
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
